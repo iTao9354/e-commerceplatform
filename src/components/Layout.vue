1acab713b8361d1ccc00270494913ad85dbd7f24
jinhaiyue <jinhaiyue@rocoinfo.com>
2017-09-28 10:35:17 +0800

    first-commit

---
+++ src/components/Layout.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="container">
+    <div class="app-head">
+      <div class="app-head-inner">
+        <img src="../assets/logo.png" alt="">
+        <div class="head-nav">
+          <ul class="nav-list">
+            <li>登录</li>
+            <li class="nav-pile">|</li>
+            <li>注册</li>
+            <li class="nav-pile">|</li>
+            <li>关于</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="app-content">
+      <keep-alive>
+        <router-view></router-view>
+      </keep-alive>
+    </div>
+    <div class="app-foot">这里是底部</div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  .container {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+  /* 头部 */
+  .app-head {
+    flex: 1;
+    background-color: #504f4f;
+    color: #b2b2b2;
+    height: 60px;
+    line-height: 60px;
+  }
+  .app-head-inner {
+    width: 1200px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+  }
+  .app-head-inner > img {
+    height: 30px;
+    margin-top: 15px;
+  }
+  .nav-list {
+    display: flex;
+  }
+  .nav-list > li:not(.nav-pile) {
+    flex: 1;
+    padding: 0 10px;
+    cursor: pointer;
+  }
+
+  /* 内容区 */
+  .app-content {
+    min-height: calc(100vh - 140px);
+    display: flex;
+    flex: 1;
+  }
+
+  /* 底部 */
+  .app-foot {
+    flex: 1;
+    height: 80px;
+    background: #e3e4e8;
+    text-align: center;
+    line-height: 80px;
+  }
+</style>
+
+
---
+++ src/pages/index.vue
@@ -0,0 +1,202 @@
+<template>
+  <div class="index-wrap">
+    <div class="index-left">
+      <div class="index-left-block">
+        <h2>全部产品</h2>
+        <div class="index-left-info" :class="{'left-last-info': product.last}" v-for="product in productList">
+          <h3>{{ product.title }}</h3>
+          <ul class="index-left-list">
+            <li v-for="item in product.list">
+              <a :href="item.url">{{ item.title }}</a>
+              <span v-if="item.hot" class="hot-tag">HOT</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="index-left-block">
+        <h2>最新消息</h2>
+        <div class="index-left-info">
+          <ul class="index-left-list">
+            <li v-for="item in newsList">
+              <a :href="item.url" :title="item.title">{{ item.title }}</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="index-right">
+      <!-- <slide-show></slide-show> -->
+      <ul class="index-board">
+        <li :class="[{'board-item-even': index%2}, 'index-board-'+item.img]" v-for="(item, index) in boardList">
+          <div class="board-item-icon"></div>
+          <div class="board-item-info">
+            <h4>{{ item.title }}</h4>
+            <p>{{ item.desc }}</p>
+            <div class="board-item-btn">
+              <a href="javascript:;">立即购买</a>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      productList: {},   // 全部产品
+      newsList: [],      // 最新消息
+      boardList: []      // 面板信息
+    }
+  },
+  created () {
+    // 初始化全部产品
+    this.initAllProducts()
+    // 初始化最新消息
+    this.initLatestNews()
+    // 初始化面板信息
+    this.initBoardList()
+  },
+  methods: {
+    // 初始化全部产品
+    initAllProducts () {
+      this.$http.post('api/productList').then(res => {
+        this.productList = res.data
+      }, err => {
+        console.log(err)
+      })
+    },
+    // 初始化最新消息
+    initLatestNews () {
+      this.$http.post('api/newsList').then(res => {
+        this.newsList = res.data
+      }, err => {
+        console.log(err)
+      })
+    },
+    // 初始化面板信息
+    initBoardList () {
+      this.$http.post('api/boardList').then(res => {
+        this.boardList = res.data
+      }, err => {
+        console.log(err)
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .index-wrap {
+    width: 1200px;
+    margin: 0 auto;
+    display: flex;
+  }
+  /* 左 */
+  .index-left {
+    flex: 240px;
+    padding: 20px 15px;
+  }
+  .index-left-block {
+    background: #fff;
+    margin-bottom: 20px;
+  }
+  .index-left-block > h2 {
+    height: 34px;
+    line-height: 34px;
+    background: #41b883;
+    color: #fff;
+    font-size: 16px;
+    padding: 0 10px;
+    font-weight: normal;
+  }
+  .index-left-info {
+    padding: 0 10px;
+    border-bottom: 1px solid #e0e0e0;
+  }
+  .left-last-info {
+    border-bottom: none;
+  }
+  .index-left-info > h3 {
+    margin-top: 15px;
+  }
+  .index-left-list {
+    padding: 10px;
+  }
+  .index-left-list > li {
+    max-width: 230px;
+    height: 24px;
+    line-height: 24px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+  .hot-tag {
+    background: red;
+    color: #fff;
+    font-size: 12px;
+  }
+
+  /* 右 */
+  .index-right {
+    flex: 19;
+    padding: 20px 0;
+  }
+  .index-board {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  .index-board > li {
+    width: calc(50% - 50px);
+    background: #fff;
+    margin: 0 20px 20px 0;
+    padding: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .index-board > .board-item-even {
+    margin-right: 0;
+  }
+  .board-item-icon {
+    flex: 2;
+    height: 100%;
+    margin: 10px 15px 0 0;
+    background-size: 100% auto;
+    background-repeat: no-repeat;
+    background-position: center center;
+  }
+  .index-board-logo0 .board-item-icon {
+    background-image: url(../assets/logo0.png);
+  }
+  .index-board-logo1 .board-item-icon {
+    background-image: url(../assets/logo0.png);
+  }
+  .index-board-logo2 .board-item-icon {
+    background-image: url(../assets/logo0.png);
+  }
+  .index-board-logo3 .board-item-icon {
+    background-image: url(../assets/logo0.png);
+  }
+  .board-item-info {
+    flex: 8;
+  }
+  .board-item-info > h4 {
+    line-height: 30px;
+  }
+  .board-item-info > p {
+    font-size: 12px;
+    margin-bottom: 10px;
+  }
+  .board-item-btn > a {
+    display: inline-block;
+    height: 20px;
+    line-height: 20px;
+    padding: 4px 12px;
+    background: #41b883;
+    color: #fff;
+    font-size: 14px;
+  }
+</style>
